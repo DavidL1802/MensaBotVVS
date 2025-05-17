89ec5f67200ce27b2af99f8ef7a6ad762c91c6fc
David Leprich <david.leprich@itm.uni-stuttgart.de>
2025-05-17 16:35:47 +0200

    add .xml requests

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
-from src.utils import findStops, requestTrip
+from src.utils import findStops, requestTrip, checkTrainDisruptions
+
+stops = findStops("Urbach")
+if(stops is not None):
+    for triple in stops:
+        # print(triple)
+        pass
+
+stops = findStops("Hauptbahnhof")
+if(stops is not None):
+    for triple in stops:
+        print(triple)
 
 stops = findStops("Universität")
-for triple in stops:
-    print(triple)
+if(stops is not None):
+    for triple in stops:
+        print(triple)
 
-requestTrip("de:08111:6555", "de:08111:6556")
+trips = requestTrip("de:08119:5789", "de:08111:6008")#, "2025-05-16T13:40:49")
+for trip in trips:
+    print(f'{trip["line"]} um {trip["departureTimeTable"]} (+{trip["departureDelay"]} min) von {trip["startLocation"]} ({trip["startPlatform"]}) -> {trip["endLocation"]} ({trip["endPlatform"]}) Ankunft um {trip["arrival"]} ({trip["duration"]})')
+
+checkTrainDisruptions("de:08119:5789", "de:08111:6008")
